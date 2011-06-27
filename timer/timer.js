1aa51bf6eba36d2283eba0a1d22da93b6fa34261
Aleksandr Motsjonov <soswow@gmail.com>
2011-06-28 01:29:27 +0300

    Enclosing variables into closed scope. Global Vars (and funcs) are evil!

--- timer/timer.js
+++ timer/timer.js
@@ -1,67 +1,65 @@
-var timerLengthMinutes = 5;
-var timerWarningMinutes = 1;
-
-function switchTo5MinutesPresentation() {
-	timerLengthMinutes = 5;
-	timerWarningMinutes = 1;
-}
-
-function switchTo40MinutesPresentation() {
-	timerLengthMinutes = 40;
-	timerWarningMinutes = 5;
-}
-
-var startTime, timer;
-function start() {
-	startTime = new Date();
-	$("#timerText").css("color", "black");
-	$("#timerBar").css("background", "green");
-	runTimer();
-	timer = setInterval(runTimer, 1000);
-	$("#startStopButton").text("stop");
-}
-
-function stop() {
-	clearInterval(timer);
-	timer = null;
-	$("#timerText").css("color", "gray");
-	$("#timerBar").css("background", "gray");
-	$("#startStopButton").text("start");
-}
-
-function startStop() {
-	if (timer) {
-		stop();
-	}
-	else {
-		start();
-	}
-}
-
-function pad(numericValue) {
-	return (numericValue < 10) ? '0' + numericValue.toString() : numericValue.toString();
-}
-
-var runTimer = function() {
-	var now = new Date();
-	var milliseconds = now.getTime() - startTime.getTime();
-	var minutesGone = milliseconds/60000;
-	
-	//var timerValue = pad(Math.floor(minutesGone)) + ":" + pad(Math.floor(milliseconds/1000)); // MIN:SEC
-	var timerValue = pad(Math.floor(minutesGone)) + " / " + pad(timerLengthMinutes);
-	$("#timerText").text(timerValue);
-
-	$("#timerBar").css("width", Math.round(milliseconds/1000/60/timerLengthMinutes * 1300));
-	if ((timerLengthMinutes - minutesGone <= timerWarningMinutes)) {
-		$("#timerBar").css("background", "red");
-		$("#timerText").css("color", "red");
-	}
-};
-
 $(function() {
-	$(window).keypress(function(event) {
-		if (event.charCode == 32 || event.charCode == 13) {
-			startStop();
-		}
-	});
+  var timerLengthMinutes = 5,
+      timerWarningMinutes = 1,
+      startTime, timer;
+
+  function switchTo5MinutesPresentation() {
+    timerLengthMinutes = 5;
+    timerWarningMinutes = 1;
+  }
+
+  function switchTo40MinutesPresentation() {
+    timerLengthMinutes = 40;
+    timerWarningMinutes = 5;
+  }
+
+  function start() {
+    startTime = new Date();
+    $("#timerText").css("color", "black");
+    $("#timerBar").css("background", "green");
+    runTimer();
+    timer = setInterval(runTimer, 1000);
+    $("#startStopButton").text("stop");
+  }
+
+  function stop() {
+    clearInterval(timer);
+    timer = null;
+    $("#timerText").css("color", "gray");
+    $("#timerBar").css("background", "gray");
+    $("#startStopButton").text("start");
+  }
+
+  function startStop() {
+    timer ? stop() : start();
+  }
+
+  function pad(numericValue) {
+    return (numericValue < 10 ? '0' : '') + numericValue.toString();
+  }
+
+  var runTimer = function() {
+    var now = new Date(),
+        milliseconds = now.getTime() - startTime.getTime(),
+        minutesGone = milliseconds / 60000,
+        timerValue = pad(Math.floor(minutesGone)) + " / " + pad(timerLengthMinutes),
+        barWidth = Math.round(milliseconds / 1000 / 60 / timerLengthMinutes * 1300);
+
+    $("#timerText").text(timerValue);
+    $("#timerBar").css("width", barWidth);
+
+    if ((timerLengthMinutes - minutesGone <= timerWarningMinutes)) {
+      $("#timerBar").css("background", "red");
+      $("#timerText").css("color", "red");
+    }
+  };
+
+  $(window).keypress(function(event) {
+    if (event.charCode == 32 || event.charCode == 13) {
+      startStop();
+    }
+  });
+  $("#5minutes").click(switchTo5MinutesPresentation);
+  $("#40minutes").click(switchTo40MinutesPresentation);
+  $("#40minutes, #5minutes").click(function(){return false;});
 });
